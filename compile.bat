69c55768385d5e851f9b22f2331d29a96a9b7d04
Beginnercodinggamer228 <gennadijsulkov@gmail.com>
2025-09-10 17:59:14 +0300

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,10 +1,8 @@
 @echo off
-echo Компиляция базового Java кода...
 mkdir build\classes 2>nul
 javac -d build\classes src\main\java\com\exemple\project\forge\*.java
 if %errorlevel% == 0 (
-    echo Компиляция успешна!
-    echo Классы находятся в build\classes\
+    echo Build Suceffuil
 ) else (
-    echo Ошибка компиляции!
+    echo Build faled!
 )
